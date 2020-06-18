6d1e7a9c4d16ac9a91d894902168e814f460a268
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-18 13:20:01 -0500

    Export Sequence diagrams

--- docs/diagrams/export/export.sh
+++ docs/diagrams/export/export.sh
@@ -1,6 +1,6 @@
-..\..\..\node_modules\.bin\mmdc -i .\D03APIQuerySequence.mmd -o D03APIQuerySequence.png -w 1024 -H 768
-..\..\..\node_modules\.bin\mmdc -i .\D04UserRegisterSequence.mmd -o D04UserRegisterSequence.png -w 1024 -H 768
-..\..\..\node_modules\.bin\mmdc -i .\D05OrganizationSequence.mmd -o D05OrganizationSequence.png -w 1024 -H 768
-..\..\..\node_modules\.bin\mmdc -i .\D06MessagingSequence.mmd -o D06MessagingSequence.png -w 1024 -H 768
-..\..\..\node_modules\.bin\mmdc -i .\D07ChannelSequence.mmd -o D07ChannelSequence.png -w 1024 -H 768
-..\..\..\node_modules\.bin\mmdc -i .\D08QuerySequence.mmd -o D08QuerySequence.png -w 1024 -H 768
+..\..\..\node_modules\.bin\mmdc -i .\D03APIQuerySequence.mmd -o D03APIQuerySequence.png -w 2000 -H 2000
+..\..\..\node_modules\.bin\mmdc -i .\D04UserRegisterSequence.mmd -o D04UserRegisterSequence.png -w 2000 -H 2000
+..\..\..\node_modules\.bin\mmdc -i .\D05OrganizationSequence.mmd -o D05OrganizationSequence.png -w 2000 -H 2000
+..\..\..\node_modules\.bin\mmdc -i .\D06MessagingSequence.mmd -o D06MessagingSequence.png -w 2000 -H 2000
+..\..\..\node_modules\.bin\mmdc -i .\D07ChannelSequence.mmd -o D07ChannelSequence.png -w 2000 -H 2000
+..\..\..\node_modules\.bin\mmdc -i .\D08QuerySequence.mmd -o D08QuerySequence.png -w 2000 -H 2000
