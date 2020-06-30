f08417a76db716760d4dce45225f6eb319a27a4c
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-30 16:53:33 -0500

    Heroku packages

--- orion_ecs/settings.py
+++ orion_ecs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -142,3 +143,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
