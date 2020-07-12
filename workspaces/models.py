6403e263f1d6917a63304b28240bf0118a040544
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-07-12 12:44:21 -0500

    Create Workspace model

--- workspaces/models.py
+++ workspaces/models.py
@@ -8,9 +8,30 @@ class Organization(models.Model):
     description = models.CharField(max_length=1000)
     admin = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        related_name='admin',
+        related_name='organization_admin',
         on_delete=models.CASCADE
     )
     people = models.ManyToManyField(
-        settings.AUTH_USER_MODEL
+        settings.AUTH_USER_MODEL,
+        related_name='organization_people'
+    )
+
+
+class Workspace(models.Model):
+    """ Workspace Model class """
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+    organization = models.ForeignKey(
+        Organization,
+        related_name='workspace_organization',
+        on_delete=models.CASCADE
+    )
+    admin = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='workspace_admin',
+        on_delete=models.CASCADE
+    )
+    people = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='workspace_people',
     )
