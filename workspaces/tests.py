861b2cb1ef34a720f4a8b343af153aeece780a83
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-08-20 14:36:46 -0500

    Unit: PU1.1: API REST

--- workspaces/tests.py
+++ workspaces/tests.py
@@ -1,173 +1,32 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from workspaces import models
+from rest_framework.test import APITestCase
 
 
-class PF1_Platform(TestCase):
+class API():
+    @staticmethod
+    def org(org_id=None):
+        if org_id is None:
+            return '/api/v1/organizations/'
+        return '/api/v1/organizations/{}/'.format(org_id)
+
+    @staticmethod
+    def wks(org_id, wks_id=None):
+        if wks_id is None:
+            return API.org(org_id) + '/workspaces/'
+        return API.org(org_id) + '/workspaces/{}/'.format(wks_id)
+
+
+class UnitTests(APITestCase):
     def setUp(self):
-        pass
+        self.username = 'testman'
+        self.password = 'testpass'
+        self.user = User.objects.create(
+            username=self.username, password=self.password)
+        self.client.force_authenticate(user=self.user)
 
-    def test_example(self):
-        """ PF1: Acceso a la plataforma """
-        self.assertEqual(1, 1)
-
-
-class PF2_Login(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PF2: Inicio de sesión """
-        self.assertEqual(1, 1)
-
-
-class PF3_Organization(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PF3: Organization """
-        self.assertEqual(1, 1)
-
-
-class PF4_Chat(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PF4: Mensajes instanáneos """
-        self.assertEqual(1, 1)
-
-
-class PF5_Channel(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PF5: Canales y mensajes """
-        self.assertEqual(1, 1)
-
-
-class PF6_Search(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PF6: Búsqueda avanzada """
-        self.assertEqual(1, 1)
-
-
-class PU1_1_UnitTest(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PU1_1: API REST """
-        self.assertEqual(1, 1)
-
-
-class PU1_2_UnitTest(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PU1_2: Microservicios """
-        self.assertEqual(1, 1)
-
-
-class PU2_1_UnitTest(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PU2_1: Registro mediante API """
-        self.assertEqual(1, 1)
-
-
-class PU2_2_UnitTest(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PU2_2: Inicio mediante API """
-        self.assertEqual(1, 1)
-
-
-class PU2_3_UnitTest(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PU2_3: Establecer apodo """
-        self.assertEqual(1, 1)
-
-
-class PU3_1_UnitTest(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PU3_1: Crear organización """
-        self.assertEqual(1, 1)
-
-
-class PU3_2_UnitTest(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PU3_2: Unirse a organización """
-        self.assertEqual(1, 1)
-
-
-class PU4_1_UnitTest(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PU4_1: Enviar mensaje """
-        self.assertEqual(1, 1)
-
-
-class PU4_2_UnitTest(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PU4_2: Recibir mensaje """
-        self.assertEqual(1, 1)
-
-
-class PU4_3_UnitTest(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PU4_3: Mensaje con adjuntos """
-        self.assertEqual(1, 1)
-
-
-class PU5_1_UnitTest(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PU5_1: Crear canal """
-        self.assertEqual(1, 1)
-
-
-class PU5_2_UnitTest(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PU5_2: Unirse a canal """
-        self.assertEqual(1, 1)
-
-
-class PU5_3_UnitTest(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        """ PU5_3: Descripción canal """
-        self.assertEqual(1, 1)
+    def test_u1_1_api_rest(self):
+        """ PU1.1: API REST """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(response.status_code, 200)
