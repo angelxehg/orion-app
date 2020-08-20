8e57f925e50fab927367f2b6c7a9d14e21b835fd
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-08-20 17:30:22 -0500

    Create all test cases

--- workspaces/tests.py
+++ workspaces/tests.py
@@ -24,7 +24,57 @@ class API():
         return API.org(org_id) + 'channels/{}/'.format(chn_id)
 
 
+class FunctionalTests(APITestCase):
+
+    def setUp(self):
+        self.username = 'functionalman'
+        self.password = 'functionalpass'
+        self.user = User.objects.create(
+            username=self.username, password=self.password)
+        self.org = models.Organization.objects.create(
+            title="Nova X", description="Functional organization", admin=self.user
+        )
+        self.wks = models.Workspace.objects.create(
+            title="Workspace", description="Functional workspace", admin=self.user, organization=self.org
+        )
+        self.chn = models.Channel.objects.create(
+            title="Channel", description="Functional channel", admin=self.user, organization=self.org
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_f1(self):
+        """ PF1: Acceso a plataforma """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_f2(self):
+        """ PF2: Inicio de sesión """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_f3(self):
+        """ PF3: Organización """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_f4(self):
+        """ PF4: Mensajes instantáneos """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_f5(self):
+        """ PF5: Canales y mensajes """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_f6(self):
+        """ PF6: Búsqueda avanzada """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+
 class UnitTests(APITestCase):
+
     def setUp(self):
         self.username = 'testman'
         self.password = 'testpass'
@@ -41,12 +91,12 @@ class UnitTests(APITestCase):
         )
         self.client.force_authenticate(user=self.user)
 
-    def test_u1_1_api_rest(self):
+    def test_u1_1(self):
         """ PU1.1: API REST """
         response = self.client.options(API.org(), format='json')
         self.assertEqual(response.status_code, 200)
 
-    def test_u1_2_services(self):
+    def test_u1_2(self):
         """ PU1.2: Microservices """
         org_url = API.org(self.org.id)
         wks_url = API.wks(self.org.id, self.wks.id)
@@ -57,3 +107,63 @@ class UnitTests(APITestCase):
         self.assertEqual(org_options.status_code, 200)
         self.assertEqual(wks_options.status_code, 200)
         self.assertEqual(chn_options.status_code, 200)
+
+    def test_u2_1(self):
+        """ PU2.1: Registro mediante API """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_u2_2(self):
+        """ PU2.2: Inicio mediante API """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_u2_3(self):
+        """ PU2.3: Establecer apodo """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_u3_1(self):
+        """ PU3.1: Crear organización """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_u3_2(self):
+        """ PU3.2: Unirse a organización """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_u4_1(self):
+        """ PU4.1: Enviar mensaje """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_u4_2(self):
+        """ PU4.2: Recibir mensaje """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_u4_3(self):
+        """ PU4.3: Mensaje con adjuntos """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_u5_1(self):
+        """ PU5.1: Crear canal """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_u5_2(self):
+        """ PU5.2: Unirse a canal """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_u5_3(self):
+        """ PU5.3: Descripción canal """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
+
+    def test_u6(self):
+        """ PU6: Búsqueda con filtros """
+        response = self.client.options(API.org(), format='json')
+        self.assertEqual(1, 1)
